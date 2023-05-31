3a9cba4354f25c9e001ce593ea6774567ef5458e
npxx <96121824+npxx@users.noreply.github.com>
2023-05-31 23:08:35 +0530

    Printed a line

--- add.cpp
+++ add.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
+using namespace std;
 
 int main()
 {
     int a= 3;
     int b = 4;
-    std::cout<<(a + b);
+    cout << (a + b);
+    cout << "I can fork and clone github repos now!\n";
+    return 0;
 }
